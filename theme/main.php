69e63f2b030a37ab6575231ba59324e96badf106
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-09-23 18:57:19 +0900

    Implement main.php

--- theme/main.php
+++ theme/main.php
@@ -1,3 +1,5 @@
+<?php $page_view_logger = new PageViewLogger(); ?>
+
 <main>
   <?php if (is_front_page()) { ?>
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
@@ -8,6 +10,18 @@
   <?php } ?>
 
   <?php if (is_single()) { ?>
+    <?php $post_id = get_the_ID(); ?>
+
+    <?php if ( !is_user_logged_in() ) { ?>
+      <?php $page_view_logger->count($post_id); ?>
+    <?php } ?>
+    <?php echo $page_view_logger->get($post_id); ?>
     <h2>Content</h2>
   <?php } ?>
+
+  <?php $page_view_logger->getPopularPosts(5); while(have_posts()) : the_post(); ?>
+    <div class="col-sm-8 col-xs-8">
+      <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+    </div>
+  <?php endwhile; ?>
 </main>
